6ee720933553f67172c6fcd53cc91fbc0095fba9
oladimejiadesina-123 <adesinaoladimeji04@gmail.com>
2022-06-22 13:36:41 +0100

    adding a new project

--- src/App.js
+++ src/App.js
@@ -2,21 +2,21 @@ import React, { useState } from 'react';
 import './App.css';
 // import Footer from './Footer/Footer';
 // import Header from './Header/Header';
-import Counter from './project/Counter';
-// import BirthdayRemembrance from './project/BirthdayRemembrance';
-// import data from './Data/data'
+// import Counter from './project/Counter';
+import BirthdayRemembrance from './project/BirthdayRemembrance';
+import data from './Data/data'
 
 
 function App() {
-  // const [people, setPeople] = useState(data)
+  const [people, setPeople] = useState(data)
   return (
     <div className="app">
-      <Counter />
-      {/* <section>
+      {/* <Counter /> */}
+      <section>
       <h3>{people.length} birthdays today</h3>
       <BirthdayRemembrance people={people}/>
       <button onClick={() => setPeople([])}>clear all</button>
-      </section> */}
+      </section>
     </div>
   );
 }
