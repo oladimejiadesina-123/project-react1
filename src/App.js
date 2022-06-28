0727228192fe4cc9a22c93728f068dea563a048a
oladimejiadesina-123 <adesinaoladimeji04@gmail.com>
2022-06-28 14:22:46 +0100

    fetching data from an api

--- src/App.js
+++ src/App.js
@@ -1,59 +1,40 @@
 import React, {  useState, useEffect } from 'react';
-import { Loading } from './Tours/Loading';
-import Tours from './Tours/Tours';
 
 
 
-const url = 'https://course-api.com/react-tours-project'
+const url = 'https://jsonplaceholder.typicode.com/comments'
 
 function App() {
-  const [loading, setLoading] = useState(true)
-  const [tours, setTours] = useState([])
-
-  const removeTour = (id) => {
-    const newTours = tours.filter((tour) => tour.id !== id)
-    setTours(newTours)
-  }
+  const [names, setNames] = useState([])
 
   const fetchTours = async () => {
-    setLoading(true)
+
     try {
       const response = await fetch(url)
-      const tours = await response.json()
-      setLoading(false)
-      setTours(tours)
+      const names = await response.json()
+      setNames(names)
+      console.log(response)
     } catch (error) {
-      setLoading(false)
       console.log(error)
     }
   }
   useEffect(() => {
     fetchTours()
   }, [])
-  if (loading) {
-    return (
-      <main>
-        <Loading />
-      </main>
-    )
-  }
-  if (tours.length === 0) {
-    return (
-      <main>
-        <div className='title'>
-          <h2>no tours left</h2>
-          <button className='btn' onClick={() => fetchTours()}>
-            refresh
-          </button>
-        </div>
-      </main>
-    )
-  }
-  return (
-    <main>
-      <Tours tours={tours} removeTour={removeTour} />
-    </main>
+const nameList = names.map((name) =>
+<div className='center'>
+
+  <li>{name.name}</li>
+  <li>{name.email}</li>
+</div>
+
+)
+
+
+  return(
+    <ul>{nameList}</ul>
   )
+
 }
 
 export default App;
